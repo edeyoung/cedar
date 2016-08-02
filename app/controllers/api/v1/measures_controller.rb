49853bbcfb8b31905e3388ba40104776504faa91
Chu <djchu@mitre.org>
2016-08-02 16:04:35 -0400

    Docs and tests for validation and measure in API

--- app/controllers/api/v1/measures_controller.rb
+++ app/controllers/api/v1/measures_controller.rb
@@ -5,10 +5,22 @@ module API
 
       respond_to :json_api
 
-      def index
-        render json: MeasureRepresenter.for_collection.new(Measure.all.top_level.only(:cms_id, :name, :description, :hqmf_id))
+      resource_description do
+        short 'Test cases'
+        formats ['json']
+        header 'X-API-EMAIL', 'user\'s email', required: true
+        header 'X-API-TOKEN', 'user\'s current authentication token', required: true
+        description <<-EOS
+        Measures used by Cedar, referred to by CMS id.
+        EOS
       end
 
+      api! 'get all measures'
+      def index
+        render json: MeasureRepresenter.for_collection.new(Measure.all.top_level.only(:tags, :cms_id, :name, :description, :hqmf_id))
+      end
+
+      api! 'show single measure'
       def show
         render json: MeasureRepresenter.new(Measure.find_by(cms_id: params[:id]))
       end
