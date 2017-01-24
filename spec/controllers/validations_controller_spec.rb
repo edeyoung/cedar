b102d887195dadee3fc10092b38c8ff5f5630661
Susan Cudmore <scudmore@mitre.org>
2017-01-24 09:48:15 -0500

    Removing old file. Fixing validation controller test.

--- spec/controllers/validations_controller_spec.rb
+++ spec/controllers/validations_controller_spec.rb
@@ -6,6 +6,9 @@ RSpec.describe API::V1::ValidationsController, type: 'controller' do
   include RSpec::Matchers
 
   before(:each) do
+    @request.env['devise.mapping'] = Devise.mappings[:user]
+    @request.headers['Accept'] = 'application/vnd.api+json'
+    @request.headers['Content-Type'] = 'application/vnd.api+json'
     setup_fixture_data
     user = create(:user)
     sign_in user
@@ -15,17 +18,17 @@ RSpec.describe API::V1::ValidationsController, type: 'controller' do
     get :index
     assert_response :success
     data = JSON.parse(response.body)
-    @validations = data['data'][0]['type']
-    expect(@validations).to_not be_nil
+    @validations = data['validations']
+    expect(@validations.length).to be >= 10 # there are 16 currently
   end
 
   it 'should show validation' do
     @v = Validation.all.first
-    get :show, id: @v.id.to_s
+    get :show, params: { id: @v.id.to_s, qrda_type: '1' }
     assert_response :success
 
     data = JSON.parse(response.body)
-  byebug
+    byebug
     @validations = data['data']['validations']
     expect(@validations).to_not be_nil
   end
